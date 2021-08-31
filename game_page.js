6c03d699c825fe01cedc4002b0b3388fadc45e8a
MAS291109 <81005913+MAS291109@users.noreply.github.com>
2021-08-31 18:53:25 +0530

    Add files via upload
    check function added

--- game_page.js
+++ game_page.js
@@ -13,9 +13,10 @@ document.getElementById("player2_score").innerHTML = player2_score;
 document.getElementById("player_question").innerHTML = "Question Turn : " + player1_name;
 document.getElementById("player_answer").innerHTML = "Answer Turn : " + player2_name;
 
+var word = "";
 function send() {
 var get_word = document.getElementById("word").value;
-var word = get_word.toLowerCase();
+word = get_word.toLowerCase();
 console.log("word in lower-case = " + word);
 
 var charAt1 = word.charAt(1);
@@ -40,4 +41,50 @@ var check_button = "<br><br><br><button class='btn btn-info' onclick='check()'>C
 var row = question_word + input_box + check_button;
 document.getElementById("output").innerHTML = row;
 document.getElementById("word").value = "";
+}
+
+var question_turn = "player1";
+var answer_turn = "player2";
+
+function check()
+{
+var get_answer = document.getElementById("input_check_box").value;
+var answer = get_answer.toLowerCase();
+console.log("lower case answer :" + answer);
+console.log("Question : " + word);
+
+if(answer == word)
+{
+    if(answer_turn == "player1")
+    {
+        player1_score = player1_score + 1;
+        document.getElementById("player1_score").innerHTML = player1_score;
+    }
+    else
+    {
+        player2_score = player2_score + 1;
+        document.getElementById("player2_score").innerHTML = player2_score;
+    }
+}
+if(question_turn == "player1")
+{
+    question_turn = "player2"
+    document.getElementById("player_question").innerHTML = "Question turn :" + player2_name;
+}
+else 
+{
+    question_turn = "player1"
+    document.getElementById("player_question").innerHTML = "Question Turn: "+ player1_name;
+}
+if(answer_turn == "player1")
+{
+    answer_turn =  "player2"
+    document.getElementById("player_answer").innerHTML = "Answer turn: " + player2_name;
+}
+else 
+{
+    answer_turn = "player1"
+    document.getElementById("player_answer").innerHTML = "Answer Turn: " + player1_name;
+}
+document.getElementById("output").innerHTML = "";
 }
